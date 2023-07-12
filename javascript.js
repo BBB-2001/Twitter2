33b6e0747c6c12b228f14113bcfa5c19a814e5d0
BBB-2001 <baris.bal@hotmail.com>
2023-07-12 12:43:43 +0300

    comment

--- javascript.js
+++ javascript.js
@@ -11,6 +11,15 @@ $(document).ready(function () {
 
   itemsArray.forEach((index) => {
     sectionHTML(index, index.id, index.likeId);
+    // sectionComment(
+    //   index.id,
+    //   index.comments,
+    //   index.comId,
+    //   index.comment,
+    //   index.comtext,
+    //   index.comdate,
+    //   index.comLikeId
+    // );
   });
 
   form.on("submit", function (e) {
@@ -53,47 +62,149 @@ $(document).ready(function () {
     const divContainer = $("<div></div>");
     divContainer.addClass("container col-lg-12 my-5 py-5");
 
-    function sectionComment(comments, comtext, comdate, comLikeId) {
+    function sectionComment(
+      id,
+      comments,
+      comId,
+      comment,
+      comtext,
+      comdate,
+      comLikeId
+    ) {
+      console.log(comments);
       const divCommentText = $("<div></div>");
+      divCommentText.attr("id", `${comId}`);
       divCommentText.addClass("comment-text container col-lg-12 my-5 py-5 ");
       divCommentText.css("background-color", "#f8f9fa");
 
-      const cText = $("<p></p>");
-      cText.attr("id", "commentid");
-      cText.addClass("mt-3 mb-4 pb-2");
-      cText.html(comtext);
+      const spanClose = $("<span></span>");
+      spanClose.attr("id", `span`);
+      spanClose.addClass("float-end");
+      spanClose.css("cursor", "pointer");
+      spanClose.text("X");
+      spanClose.on("click", function () {
+        let postId = $(this).parent("div").attr("id");
 
-      const comlinkLike = $("<a></a>");
-      comlinkLike.attr("href", "#!");
-      comlinkLike.attr("id", `${comLikeId}`);
-      comlinkLike.addClass("d-flex align-items-center me-3");
-      comlinkLike.on("click", function () {
-        // let likeId = $(this).;
+        let items = JSON.parse(localStorage.getItem("items")) || [];
+
+        let updatedItems = items.filter(function (item) {
+          console.log(item.id, typeof item.id);
+          console.log(postId, typeof postId);
+          return item.id !== +postId;
+        });
+        console.log(updatedItems);
+        itemsArray = updatedItems;
+        localStorage.setItem("items", JSON.stringify(updatedItems));
+        $(this).closest("div").remove();
+      });
+
+      const divRow = $("<div></div>");
+      divRow.addClass("row d-flex justify-content-center");
+
+      const divCol = $("<div></div>");
+      divCol.addClass("col-md-12 col-lg-10 col-xl-8");
+
+      const divProfile = $("<div></div>");
+      divProfile.addClass("profile");
+
+      const divProfileBody = $("<div></div>");
+      divProfileBody.addClass("profile-body");
+
+      const divFlex = $("<div></div>");
+      divFlex.addClass("d-flex flex-start align-items-center");
+
+      const imgAvatar = $("<img>");
+      imgAvatar.attr("src", tweet.profilePic);
+      imgAvatar.attr("alt", "avatar");
+      imgAvatar.attr("width", "60");
+      imgAvatar.attr("height", "60");
+      imgAvatar.addClass("rounded-circle shadow-1-strong me-3");
+
+      const divProfileInfo = $("<div></div>");
+      const h6Name = $("<h6></h6>");
+      h6Name.addClass("fw-bold text-primary mb-1");
+      h6Name.html(tweet.name);
+      const divDateTime = $("<div></div>");
+      divDateTime.attr("id", "DateTime");
+      divDateTime.addClass("text-muted small mb-0");
+      divDateTime.html(comments);
+
+      divProfileInfo.append(h6Name);
+      divProfileInfo.append(divDateTime);
+
+      divFlex.append(imgAvatar);
+      divFlex.append(divProfileInfo);
+
+      const pText = $("<p></p>");
+      pText.attr("id", "textid");
+      pText.addClass("mt-3 mb-4 pb-2");
+      pText.html(comment.comtext);
+
+      const divSmall = $("<div></div>");
+      divSmall.addClass("small d-flex justify-content-start ");
+
+      const linkLike = $("<a></a>");
+      linkLike.attr("href", "#!");
+      linkLike.attr("id", `${comLikeId}`);
+      linkLike.addClass("d-flex align-items-center me-3");
+      linkLike.on("click", function () {
+        let likeId = $(this).attr("id");
 
         const tweet = itemsArray.find((item) => item.likeId === +likeId);
 
         if (tweet) {
           tweet.like++;
-          $(compLike).text("Like: " + tweet.like);
+          $(pLike).text("Like: " + tweet.like);
 
           localStorage.setItem("items", JSON.stringify(itemsArray));
         }
       });
 
-      const comiLike = $("<i></i>");
-      comiLike.addClass("far fa-thumbs-up me-2");
+      const iLike = $("<i></i>");
+      iLike.addClass("far fa-thumbs-up me-2");
 
-      const compLike = $("<p></p>");
-      compLike.addClass("like mb-0");
-      compLike.text("Like: ");
-      compLike.html("Like: " + comLikeId);
+      const pLike = $("<p></p>");
+      pLike.addClass("like mb-0");
+      pLike.text("Like: ");
+      pLike.html("Like: " + tweet.like);
 
-      comlinkLike.append(comiLike);
-      comlinkLike.append(compLike);
+      linkLike.append(iLike);
+      linkLike.append(pLike);
 
-      divCommentText.append(cText);
-      divCommentText.append(comiLike);
-      console.log(cText);
+      divSmall.append(linkLike);
+
+      divProfileBody.append(divFlex);
+      divProfileBody.append(pText);
+      divProfileBody.append(divSmall);
+
+      const divCardFooter = $("<div></div>");
+      divCardFooter.addClass("card-footer py-3 border-0 mr-3");
+      divCardFooter.css("background-color", "#f8f9fa");
+
+      const divFooterFlex = $("<div></div>");
+      divFooterFlex.addClass("d-flex flex-start w-100 mr-3");
+
+      const imgFooterAvatar = $("<img>");
+      imgFooterAvatar.attr("src", tweet.profilePic);
+      imgFooterAvatar.attr("alt", "avatar");
+      imgFooterAvatar.attr("width", "40");
+      imgFooterAvatar.attr("height", "40");
+      imgFooterAvatar.addClass("rounded-circle shadow-1-strong me-2");
+
+      divProfile.append(divProfileBody);
+      divProfile.append(divCardFooter);
+
+      divCol.append(divProfile);
+
+      divRow.append(divCol);
+
+      divCommentText.append(spanClose);
+      divCommentText.append(divRow);
+
+      // section select
+      // divComment append to section
+      const parentSection = $(`#${id}`);
+      parentSection.append(divCommentText);
     }
 
     const spanClose = $("<span></span>");
@@ -240,7 +351,7 @@ $(document).ready(function () {
     btnShare.text("Paylaş");
     btnShare.on("click", function (comments) {
       const textareaValue = $("#textAreaExample").val();
-
+      const comId = idGen.getId();
       const comLikeId = idGen.getId();
       const date = new Date();
       const year = date.getFullYear();
@@ -249,24 +360,16 @@ $(document).ready(function () {
       const hour = date.getHours();
       const minute = date.getMinutes();
       const comment = {
+        comId,
         comtext: textareaValue,
         comdate: [day, "/", month, "/", year, "  ", hour, ":", minute],
-        comLikeId: 0,
+        comLikeId,
+        comlike: 0,
       };
 
-      // }
-      const postId = $(this)
-        .parent("div")
-        .parent("div")
-        .parent("div")
-        .parent("div")
-        .parent("div")
-        .parent("div")
-        .parent("section")
-        .attr("id");
-      const tweet = itemsArray.find((item) => item.id === +postId);
+      const tweet = itemsArray.find((item) => item.id === +id);
       itemsArray.find((item) => console.log(item.id));
-      console.log(+postId);
+      console.log(+id);
 
       // if (tweet) {
       //   tweet.comments.push(comment);
@@ -279,7 +382,14 @@ $(document).ready(function () {
         localStorage.setItem("items", JSON.stringify(itemsArray));
         textareaComment.val("");
         console.log("Yorum paylaşıldı: ", comment);
-        sectionComment(comment.comtext, comment.comdate, comment.comLikeId);
+
+        sectionComment(
+          id,
+          comments,
+          comment.comId,
+          comment.comtext,
+          comment.comLikeId
+        );
       }
     });
 
