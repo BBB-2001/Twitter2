675b2b828feb2db55e7e47f2c8c7821093b61a01
BBB-2001 <baris.bal@hotmail.com>
2023-07-19 17:17:18 +0300

    databesde commet olayı çözüldü

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   const token = localStorage.getItem("token");
-  console.log(token);
 
   if (!token) {
     window.location.href = "login.html";
@@ -9,15 +8,9 @@ $(document).ready(function () {
   const form = $("form");
   const div = $("#textid");
   const button1 = $("#button-1");
-  const data = {
-    post,
-  };
-  const input = $.post("http://192.168.1.18:3001/tweets", data)
-    .done(function (response) {})
-    .fail(function () {
-      alert("Giriş başarısız oldu.");
-    });
-  const itemsArray = $();
+  const input = $("#item");
+  const itemsArray = [];
+  let arr = [];
 
   const res = $.ajax({
     url: "http://192.168.1.18:3001/authenticated",
@@ -36,44 +29,46 @@ $(document).ready(function () {
             Authorization: `bearer ${token}`,
           },
           success: function (response) {
+            console.log(response);
             const tweets = response.posts;
+            tweets.reverse();
             tweets.forEach((tweet) => {
               const processedTweet = {
-                id: tweet.id,
-                likeId: tweet.comments.id,
-                name: tweet.user,
-                profilePic: tweet.porofile_photo,
-                date: created_at,
-                text: tweet.text,
+                id: tweet.post_id,
+                likeId: tweet.like_count,
+                name: tweet.username,
+                profilePic: tweet.profile_photo,
+                date: tweet.created_at,
+                text: tweet.post_text,
                 comments: tweet.comments,
                 like: tweet.like_count,
               };
-              sectionHTML(processedTweet);
-              tweet.comments.forEach((item) => {
-                const comment = {
-                  id: tweet.id,
-                  comId: item.id,
-                  name: item.user,
-                  profilePic: item.porofile_photo,
-                  date: created_at,
-                  text: item.text,
-                  comtext: item.comments,
-                  like: item.like_count,
-                };
-                sectionComment(
-                  tweet.id,
-                  item.comId,
-                  item.comLike,
-                  item.comtext,
-                  item.comdate,
-                  item.comLikeId,
-                  item.profilePic,
-                  item.name
-                );
-              });
-            });
+              arr = [...arr, processedTweet];
 
-            console.log(response);
+              sectionHTML(processedTweet, tweet.post_id, tweet.like_count);
+              // tweets.forEach((item) => {
+              //   const comment = {
+              //     id: tweet.post_id,
+              //     comId: item.id,
+              //     name: item.username,
+              //     profilePic: item.profile_photo,
+              //     date: item.created_at,
+              //     text: item.comment_text,
+              //     comtext: item.comments,
+              //     like: item.like_count,
+              //   };
+              //   sectionComment(
+              //     tweet.post_id,
+              //     item.id,
+              //     item.comLike,
+              //     item.comment_text,
+              //     item.created_at,
+              //     item.comLikeId,
+              //     item.profile_photo,
+              //     item.username
+              //   );
+              // });
+            });
           },
           error: function (error) {
             // İstek başarısız olduğunda yapılacak işlemler
@@ -87,34 +82,49 @@ $(document).ready(function () {
       window.location.href = "login.html";
     },
   });
-
   form.on("submit", function (e) {
     e.preventDefault();
-
-    // Tweet metni ve diğer gerekli bilgilerin alınması
-    var tweetText = input.val();
-    var tweetId = tweet.id; // tweet id'si
-    var tweetLikeId = tweet.like; // tweet like id'si
-
-    if (tweetText !== "") {
-      // Yapılacak işlemler (örneğin, verilerin kaydedilmesi veya gösterilmesi)
-      var tweet = {
-        text: tweetText,
-        id: tweetId,
-        likeId: tweetLikeId,
-      };
-
-      input.val("");
-      sectionHTML(tweet, tweetId, tweetLikeId);
-    }
+    const data = {
+      post_text: input.val(),
+    };
+    $.ajax({
+      url: "http://192.168.1.18:3001/tweets", // Veriyi göndereceğiniz adres
+      method: "POST", // HTTP POST isteği yapılıyor
+      data: data, // Gönderilecek veri
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader("Authorization", `bearer ${token}`);
+      },
+      success: function (response) {
+        console.log(response);
+        const processedTweet = {
+          id: response.post.post_id,
+          likeId: response.post.like_count,
+          name: response.post.username,
+          profilePic: "image/blank-profile-picture-973460_960_720.webp",
+          date: response.post.created_at,
+          text: response.post.post_text,
+          comments: response.post.comments,
+          like: response.post.like_count,
+        };
+        if (input.val() !== "") {
+          // Yapılacak işlemler (örneğin, verilerin kaydedilmesi veya gösterilmesi)
+          console.log(input);
+          input.val("");
+          sectionHTML(
+            processedTweet,
+            response.post.post_id,
+            response.post.like_count
+          );
+        }
+        console.log("Veri başarıyla gönderildi");
+        // Gönderme işleminden sonra yapılacak işlemleri buraya ekleyebilirsiniz
+      },
+      error: function (xhr, status, error) {
+        // Gönderme sırasında bir hata oluştuğunda çalışacak işlev
+        console.error("Veri gönderme hatası:", error);
+      },
+    });
   });
-
-  // Sayfa yenilendiğinde verileri yükle
-  $(document).ready(function () {
-    // localStorage'dan verilerin alınması ve gerekli işlemlerin yapılması (örneğin, sectionHTML() çağrısı)
-  });
-
-  // Sayfa yenilendiğinde verileri yükle
 });
 
 function sectionComment(
@@ -276,7 +286,7 @@ function sectionComment(
   parentSection.append(divCommentText);
 }
 
-function sectionHTML(tweet, id, likeId) {
+function sectionHTML(processedTweet, id, likeId) {
   const section = $("<section></section>");
   section.attr("id", `${id}`);
   section.css("background-color", "#eee");
@@ -290,19 +300,22 @@ function sectionHTML(tweet, id, likeId) {
   spanClose.addClass("float-end");
   spanClose.css("cursor", "pointer");
   spanClose.text("X");
-  spanClose.on("click", function () {
-    let postId = $(this).parent("div").parent("section").attr("id");
-
-    let items = JSON.parse(localStorage.getItem("items")) || [];
-
-    let updatedItems = items.filter(function (item) {
-      console.log(item.id, typeof item.id);
-      console.log(postId, typeof postId);
-      return item.id !== +postId;
+  spanClose.on("click", function (e) {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    $.ajax({
+      url: `http://192.168.1.18:3001/tweets/${id}`,
+      method: "DELETE",
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader("Authorization", `bearer ${token}`);
+      },
+      success: function (response) {
+        console.log("Veri başarıyla silindi");
+      },
+      error: function (xhr, status, error) {
+        console.error("Veri silme hatası:", error);
+      },
     });
-    console.log(updatedItems);
-    itemsArray = updatedItems;
-    localStorage.setItem("items", JSON.stringify(updatedItems));
     $(this).closest("section").remove();
   });
 
@@ -322,7 +335,7 @@ function sectionHTML(tweet, id, likeId) {
   divFlex.addClass("d-flex flex-start align-items-center");
 
   const imgAvatar = $("<img>");
-  imgAvatar.attr("src", tweet.profilePic);
+  imgAvatar.attr("src", processedTweet.profilePic);
   imgAvatar.attr("alt", "avatar");
   imgAvatar.attr("width", "60");
   imgAvatar.attr("height", "60");
@@ -331,11 +344,11 @@ function sectionHTML(tweet, id, likeId) {
   const divProfileInfo = $("<div></div>");
   const h6Name = $("<h6></h6>");
   h6Name.addClass("fw-bold text-primary mb-1 d-flex ");
-  h6Name.html(tweet.name);
+  h6Name.html(processedTweet.name);
   const divDateTime = $("<div></div>");
   divDateTime.attr("id", "DateTime");
   divDateTime.addClass("text-muted small mb-0");
-  divDateTime.html(tweet.date);
+  divDateTime.html(processedTweet.date);
 
   divProfileInfo.append(h6Name);
   divProfileInfo.append(divDateTime);
@@ -346,7 +359,7 @@ function sectionHTML(tweet, id, likeId) {
   const pText = $("<p></p>");
   pText.attr("id", "textid");
   pText.addClass("mt-3 mb-4 pb-2 text-break");
-  pText.html(tweet.text);
+  pText.html(processedTweet.text);
 
   const divSmall = $("<div></div>");
   divSmall.addClass("small d-flex justify-content-start ");
@@ -355,18 +368,7 @@ function sectionHTML(tweet, id, likeId) {
   linkLike.attr("href", "#!");
   linkLike.attr("id", `${likeId}`);
   linkLike.addClass("d-flex align-items-center me-3");
-  linkLike.on("click", function () {
-    let likeId = $(this).attr("id");
-
-    const tweet = itemsArray.find((item) => item.likeId === +likeId);
-
-    if (tweet) {
-      tweet.like++;
-      $(pLike).text("Like: " + tweet.like);
-
-      localStorage.setItem("items", JSON.stringify(itemsArray));
-    }
-  });
+  linkLike.on("click", function () {});
 
   const iLike = $("<i></i>");
   iLike.addClass("far fa-thumbs-up me-2");
@@ -374,7 +376,7 @@ function sectionHTML(tweet, id, likeId) {
   const pLike = $("<p></p>");
   pLike.addClass("like mb-0");
   pLike.text("Like: ");
-  pLike.html("Like: " + tweet.like);
+  pLike.html("Like: " + processedTweet.like);
 
   linkLike.append(iLike);
   linkLike.append(pLike);
@@ -393,7 +395,7 @@ function sectionHTML(tweet, id, likeId) {
   divFooterFlex.addClass("d-flex flex-start w-100 mr-3");
 
   const imgFooterAvatar = $("<img>");
-  imgFooterAvatar.attr("src", tweet.profilePic);
+  imgFooterAvatar.attr("src", processedTweet.profilePic);
   imgFooterAvatar.attr("alt", "avatar");
   imgFooterAvatar.attr("width", "40");
   imgFooterAvatar.attr("height", "40");
@@ -403,7 +405,7 @@ function sectionHTML(tweet, id, likeId) {
   divTextarea.addClass("form-outline w-100 mr-3");
 
   const textareaComment = $("<textarea></textarea>");
-  textareaComment.attr("id", "textAreaExample");
+  textareaComment.attr("id", `textAreaExample-${id}`);
   textareaComment.addClass("form-control mr-3");
   textareaComment.attr("rows", "4");
   textareaComment.attr("style", "background: #fff; resize: none");
@@ -428,53 +430,50 @@ function sectionHTML(tweet, id, likeId) {
   btnShare.addClass("btn btn-primary btn-sm");
   btnShare.text("Paylaş");
   btnShare.on("click", function () {
-    const textareaValue = $("#textAreaExample").val();
-    const comId = idGen.getId();
-    const comLikeId = idGen.getId();
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    const comment = {
-      comId,
-      comtext: textareaValue,
-      comdate: [day, "/", month, "/", year, "  ", hour, ":", minute],
-      comLikeId,
-      comLike: 0,
-      name: "Baran Barış Bal",
-      profilePic: "image/blank-profile-picture-973460_960_720.webp",
+    const token = localStorage.getItem("token");
+    const textareaValue = $(`#textAreaExample-${id}`);
+    console.log(textareaValue);
+    const data = {
+      comment_text: textareaValue.val(),
+      post_id: id,
     };
+    $.ajax({
+      url: `http://192.168.1.18:3001/comments`,
+      method: "POST",
+      data: data,
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader("Authorization", `bearer ${token}`);
+      },
+      success: function (response) {
+        console.log(response);
+        const comment = {
+          comId: response.comment.post_id,
+          comtext: response.comment.comment_text,
+          comdate: response.comment.created_at,
+          name: response.comment.username,
+          profilePic: "image/blank-profile-picture-973460_960_720.webp",
+        };
 
-    const tweet = itemsArray.find((item) => item.id === +id);
-    itemsArray.find((item) => console.log(item.id));
-    console.log(+id);
-
-    // if (tweet) {
-    //   tweet.comments.push(comment);
-    //   localStorage.setItem("items", JSON.stringify(itemsArray));
-    //   console.log("Yorum paylaşıldı: ", comment);
-    // }
-    if (comment.comtext !== "") {
-      console.log(tweet);
-      tweet.comments.push(comment);
-      localStorage.setItem("items", JSON.stringify(itemsArray));
-      textareaComment.val("");
-      console.log("Yorum paylaşıldı: ", comment);
-
-      sectionComment(
-        id,
-
-        comment.comId,
-        comment.comLike,
-        comment.comtext,
-        comment.comdate,
-        comment.comLikeId,
-        comment.profilePic,
-        comment.name
-      );
-    }
+        if (textareaValue.val() !== "") {
+          sectionComment(
+            id,
+            comment.comId,
+            comment.comLike,
+            comment.comtext,
+            comment.date,
+            comment.comLikeId,
+            comment.profilePic,
+            comment.name
+          );
+          textareaValue.val("");
+          console.log(textareaValue);
+        }
+        console.log("Veri başarıyla gönderildi");
+      },
+      error: function (xhr, status, error) {
+        console.error("Veri gönderme hatası:", error);
+      },
+    });
   });
 
   divFooterButtons.append(btnShare);
